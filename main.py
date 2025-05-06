36958380789d1ebd2cfd09433fe523df3240b73f
id40 <id40.jay@gmail.com>
2025-05-06 20:13:29 +0530

    now able to speak the reasult provided by the LLM.

--- main.py
+++ main.py
@@ -2,21 +2,27 @@ from whisper_mic import WhisperMic
 import google.generativeai as genai
 from dotenv import load_dotenv
 import os
-
-
+import pyttsx3
 
 
 load_dotenv()
+engine = pyttsx3.init()
 
-genai.configure(api_key="")
-model = genai.GenerativeModel(model_name='gemini-2.5-pro-latest')
+genai.configure(api_key="s")
+model = genai.GenerativeModel(model_name='gemini-1.5-pro-latest')
 his= model.start_chat(history=[])
 
 def get_gemimi_response(res) :
     his.send_message(res)
     gemini_reply = his.last.text
-    print("Assistent",gemini_reply)
+   # print("Assistent",gemini_reply)
     return gemini_reply
+
+
+def response_audio(res):
+    result= get_gemimi_response(res)
+    engine.say(result)
+    engine.runAndWait()
 
 
 
@@ -28,7 +34,8 @@ while True:
         print("Listning to you ...")
         res= result
         print("You",res)
-        get_gemimi_response(res)
+        print("Assistent:",get_gemimi_response(res))
+        response_audio(res)
         continue 
 
 
